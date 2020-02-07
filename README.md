6157ea1ee6f19c23b38c4c22d7e7945d46f25ccf
Johan Bakøy <johan.bakoy99@gmail.com>
2020-02-07 15:29:22 +0100

    Cleaned up README

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# INF112 Maven template 
-Simple skeleton with libgdx. 
+# INF112 Robo Rally
+Staring-horse sin java implementasjon av robo rally
 
 
 ## Known bugs
--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -7,7 +7,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 public class Main {
     public static void main(String[] args) {
         LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-        cfg.title = "Robert rrallly";
+        cfg.title = "Robo rally";
         cfg.width = 500;
         cfg.height = 500;
 
