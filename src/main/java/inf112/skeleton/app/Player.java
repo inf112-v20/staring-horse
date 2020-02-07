d8eb74e3c8e212bab89633f9d625f7001d3fa4fe
Olav <nybo.olav@hotmail.no>
2020-02-07 13:09:20 +0100

    Make Travis work by deleting bad import

--- src/main/java/inf112/skeleton/app/Player.java
+++ src/main/java/inf112/skeleton/app/Player.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
-import com.sun.scenario.effect.impl.prism.PrImage;
 
 public class Player {
 
