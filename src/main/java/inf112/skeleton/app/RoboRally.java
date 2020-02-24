f1101e0b680482e4661804422370dea5f4c183fd
Olav <nybo.olav@hotmail.no>
2020-02-24 15:23:05 +0100

    Fix merge-problems

--- src/main/java/inf112/skeleton/app/RoboRally.java
+++ src/main/java/inf112/skeleton/app/RoboRally.java
@@ -5,24 +5,17 @@ import com.badlogic.gdx.Game;
 public class RoboRally extends Game {
 
     public GameScreen gameScreen;
-<<<<<<< Updated upstream
-=======
     public MainMenuScreen menuScreen;
->>>>>>> Stashed changes
 
     @Override
     public void create() {
         gameScreen = new GameScreen(this);
-<<<<<<< Updated upstream
-        setScreen(gameScreen);
-    }
-=======
         menuScreen = new MainMenuScreen(this);
+
         setScreen(gameScreen);
     }
 
     public void render() {
         super.render();
     }
->>>>>>> Stashed changes
 }
