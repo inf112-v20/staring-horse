82790c633b571d4668171a3b200403d49a3125ff
Olav <nybo.olav@hotmail.no>
2020-02-24 14:01:28 +0100

    Change to Screen. Add ProgramCard and CardStack

---
+++ src/main/java/inf112/skeleton/app/GameScreen.java
@@ -0,0 +1,49 @@
+package inf112.skeleton.app;
+
+import com.badlogic.gdx.Screen;
+
+public class GameScreen implements Screen {
+
+    private final RoboRally roboRally;
+
+    public GameScreen(RoboRally roboRally){
+        this.roboRally = roboRally;
+
+        
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float v) {
+
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
