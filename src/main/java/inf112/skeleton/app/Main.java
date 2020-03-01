9c45cc4f0de328c69ed0234a58c37a5519284c5d
Olav <nybo.olav@hotmail.no>
2020-03-01 17:19:05 +0100

    Set Game window position

--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -3,14 +3,21 @@ package inf112.skeleton.app;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
+import java.awt.*;
+
 
 public class Main {
     public static void main(String[] args) {
         LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
         cfg.title = "Robo Rally";
-        cfg.width = 700;
-        cfg.height = 700;
+
         // TODO change screen size for each map.
+        cfg.width = 800;
+        cfg.height = 800;
+
+        cfg.x = ((int) Toolkit.getDefaultToolkit().getScreenSize().getWidth())/2 - cfg.width/2;
+        cfg.y = 0;
+
         new LwjglApplication(new RoboRally(), cfg);
     }
 }
