9b795ac659a4bed8b25e67fdfcadd60c3613cd06
terje <terjefchristiansen@gmail.com>
2020-04-22 12:23:21 +0200

    Add takeDamage to IRobot Robots can now take damage from robot laser

--- src/main/java/inf112/skeleton/app/robot/Player.java
+++ src/main/java/inf112/skeleton/app/robot/Player.java
@@ -342,6 +342,11 @@ public class Player implements IRobot {
     public int getXPos() { return xPos; }
 
     @Override
+    public void takeDamage() {
+        this.healthPoints--;
+    }
+
+    @Override
     public void setRespawnPoint(int x, int y) {
         respawnXPos = x;
         respawnYPos = y;
